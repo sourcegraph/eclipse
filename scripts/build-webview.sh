8d2d9eb70f614ea83c93d3941ebb199457cc491a
Olafur Geirsson <olafurpg@gmail.com>
2024-06-07 13:53:20 +0200

    Use pnpm dlx to not rely on global installation

--- scripts/build-webview.sh
+++ scripts/build-webview.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eux
 pushd cody
-pnpm install
-pnpm build
-pnpm -C vscode build
+pnpm dlx pnpm@8.6.7 install
+pnpm dlx pnpm@8.6.7 build
+pnpm dlx pnpm@8.6.7 -C vscode build
 popd
 mkdir -p plugins/cody-chat/resources/cody-webviews
 cp cody/vscode/dist/webviews/* plugins/cody-chat/resources/cody-webviews
