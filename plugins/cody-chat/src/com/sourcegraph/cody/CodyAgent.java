eb9989a8be582d1f720ebdcedba73fb1afd75fc5
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-06-13 15:02:07 +0200

    WIP

--- plugins/cody-chat/src/com/sourcegraph/cody/CodyAgent.java
+++ plugins/cody-chat/src/com/sourcegraph/cody/CodyAgent.java
@@ -1,5 +1,9 @@
 package com.sourcegraph.cody;
 
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.lsp4j.jsonrpc.Launcher;
+import org.eclipse.ui.services.IDisposable;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
@@ -8,30 +12,92 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import org.eclipse.lsp4j.jsonrpc.Launcher;
-import org.eclipse.ui.services.IDisposable;
+import java.util.concurrent.Future;
 
 public class CodyAgent implements IDisposable {
 
+  private final Future<Void> listening;
+  private final CodyAgentClient client;
+  private final CodyAgentServer server;
+  private final Process process;
+
+  public CodyAgent(
+      Future<Void> listening, CodyAgentClient client, CodyAgentServer server, Process process) {
+    this.listening = listening;
+    this.client = client;
+    this.server = server;
+    this.process = process;
+  }
+
+
+@Nullable public static volatile CodyAgent AGENT = null;
+
   private static ExecutorService executorService = Executors.newCachedThreadPool();
 
-  public void stop() {
+  public static void stop() {
     System.out.println("Cody is stopping");
+    if (AGENT != null) {
+      AGENT.dispose();
+    }
+  }
+
+  private boolean isRunning() {
+    return !listening.isDone();
+  }
+
+  @Override
+  public void dispose() {
+    if (!isRunning()) {
+      return;
+    }
+    try {
+//      client
+      listening.cancel(true);
+    } catch (Exception e) {
+      System.out.println("CodyAgent.stop():");
+      e.printStackTrace();
+    }
+  }
+
+  public static CodyAgent restart() throws IOException {
+    stop();
+    return start();
   }
 
   public static CodyAgent start() throws IOException {
+    if (AGENT != null && AGENT.isRunning()) {
+      return AGENT;
+    }
+    System.out.println("Cody is restarting");
     CodyAgentClient client = new CodyAgentClient();
-    Process process = new ProcessBuilder("node", "/Users").start();
-    new Launcher.Builder<CodyAgentServer>()
-        .setRemoteInterface(CodyAgentServer.class)
-        .traceMessages(traceWriter())
-        .setExecutorService(executorService)
-        .setInput(process.getInputStream())
-        .setOutput(process.getOutputStream())
-        .setLocalService(client)
-        .create();
+    String workingDirectory = System.getProperty("user.dir");
+    System.out.println("WORKING DIR " + workingDirectory);
+    Path agentPath =
+        Paths.get(System.getProperty("user.home"))
+            .resolve("dev")
+            .resolve("sourcegraph")
+            .resolve("eclipse")
+            .resolve("cody")
+            .resolve("agent")
+            .resolve("dist")
+            .resolve("index.js");
+    Process process =
+        new ProcessBuilder("node", "--enable-source-maps", agentPath.toString())
+            .directory(Paths.get(workingDirectory).toFile())
+            .start();
+
+    Launcher<CodyAgentServer> launcher =
+        new Launcher.Builder<CodyAgentServer>()
+            .setRemoteInterface(CodyAgentServer.class)
+            .traceMessages(traceWriter())
+            .setExecutorService(executorService)
+            .setInput(process.getInputStream())
+            .setOutput(process.getOutputStream())
+            .setLocalService(client)
+            .create();
+    Future<Void> listening = launcher.startListening();
     System.out.println("Cody is starting2");
-    return new CodyAgent();
+    return new CodyAgent(listening, client, launcher.getRemoteProxy(), process);
   }
 
   private static PrintWriter traceWriter() {
@@ -49,11 +115,5 @@ public class CodyAgent implements IDisposable {
       }
     }
     return null;
-  }
-
-  @Override
-  public void dispose() {
-    // TODO Auto-generated method stub
-
   }
 }
