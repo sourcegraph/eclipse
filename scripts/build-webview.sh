cd988c007a0d2737f163acba188f5be10b1b5cac
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-06-17 12:02:38 +0200

    Remove Cody submodule
    Eclipse started freezing during startup after I ran `pnpm install` in
    the cody submodule. This PR removes the submodule in favor of requiring
    contributors to clone the Cody repo as a sibling directory. This makes
    Eclipse boot up quickly again.

--- scripts/build-webview.sh
+++ scripts/build-webview.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -eux
-git submodule update --init --recursive
-pushd cody
+pushd ../cody
 pnpm dlx pnpm@8.6.7 install
 pnpm dlx pnpm@8.6.7 build
 pnpm dlx pnpm@8.6.7 -C vscode build
 popd
 mkdir -p plugins/cody-chat/resources/cody-webviews
-cp cody/vscode/dist/webviews/* plugins/cody-chat/resources/cody-webviews
+cp ../cody/vscode/dist/webviews/* plugins/cody-chat/resources/cody-webviews
