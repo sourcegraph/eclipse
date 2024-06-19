6f2f3df1228a9ab72dd0554959ab636846b525f4
Ólafur Páll Geirsson <olafurpg@gmail.com>
2024-06-19 16:10:34 +0200

    WIP

--- scripts/build-agent.sh
+++ scripts/build-agent.sh
@@ -9,3 +9,9 @@ popd
 mkdir -p plugins/cody-chat/resources/cody-agent
 cp ../cody/agent/dist/index.js plugins/cody-chat/resources/cody-agent
 cp ../cody/agent/dist/*.wasm   plugins/cody-chat/resources/cody-agent
+cp ../cody/agent/dist/win-ca-roots.exe   plugins/cody-chat/resources/cody-agent
+ASSETS_FILE=plugins/cody-chat/resources/cody-agent/assets.txt
+rm -f $ASSETS_FILE
+for f in plugins/cody-chat/resources/cody-agent/*; do
+  echo $(basename $f) >> $ASSETS_FILE
+done
