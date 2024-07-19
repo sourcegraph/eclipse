3a918cf88aa43966dd3db98cba6b26d88922eee1
Olafur Geirsson <olafurpg@gmail.com>
2024-07-19 13:33:04 +0200

    Document synchronization: implement basic support
    This PR implements the first iteration towards giving Cody codebase
    context in Eclipse:
    
    - `textDocument/didOpen`
    - `textDocument/didFocus`
    - `textDocument/didChange`
    
    There are a few remaining cases:
    
    - `textDocument/didClose`
    - `textDocument/didOpen` on agent restart
    - `textDocument/did{Open,Change}` before agent starts
    
    Co-authored-By: Paweł Marks <pmarks@virtuslab.com>

---
+++ plugins/cody-chat/src/com/sourcegraph/cody/CodyChatActivator.java
@@ -0,0 +1,40 @@
+package com.sourcegraph.cody;
+
+import static java.lang.System.out;
+
+import com.sourcegraph.cody.workspace.WorkspaceListener;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IWorkbenchPartReference;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class CodyChatActivator extends AbstractUIPlugin {
+
+  @Override
+  public void start(BundleContext bundleContext) throws Exception {
+    // async exec to make sure the UI is initialized
+    Display.getDefault()
+        .asyncExec(
+            () -> {
+              @Nullable
+              IWorkbenchWindow workbenchWindow =
+                  PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+
+              if (workbenchWindow == null) {
+                System.out.println("No active workbench window");
+                return;
+              }
+
+              var partService = workbenchWindow.getPartService();
+
+              partService.addPartListener(new WorkspaceListener());
+
+              // Uncomment below to debug the workspace listener.
+              // partService.addPartListener(new DebugListener());
+            });
+  }
+}
