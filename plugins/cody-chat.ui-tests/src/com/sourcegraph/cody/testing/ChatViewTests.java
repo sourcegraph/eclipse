f7554208ce95e4030bb1d9601c3a415e376cdf2a
Paweł Marks <pmarks@virtuslab.com>
2024-09-27 15:47:23 +0200

    Add test target to bazel

--- plugins/cody-chat.ui-tests/src/com/sourcegraph/cody/testing/ChatViewTests.java
+++ plugins/cody-chat.ui-tests/src/com/sourcegraph/cody/testing/ChatViewTests.java
@@ -10,25 +10,27 @@ import org.junit.runner.RunWith;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class ChatViewTests {
-	private static SWTWorkbenchBot bot;
-	
-	@BeforeClass
-	public static void prepareBot() {
-		bot = new SWTWorkbenchBot();
-		bot.viewByTitle("Welcome").close();
-	}
-	
-	@Test
-	public void opened_view_should_load_a_browser() {
-		bot.menu("Window").menu("Show View").menu("Other...").click();
-		var shell = bot.shell("Show View");
-		shell.activate();
-		bot.tree().expandNode("Cody").select("Cody Chat");
-		bot.button("Open").click();
-		
-		bot.sleep(3_000); // wait 3 seconds
-		var browser = bot.browser(); // there shouldn't be any other browser widget so we can choose the first encountered
-		assertTrue(browser.isPageLoaded());
-		assertTrue(browser.getText().contains("<title>Cody</title>"));
-	}
+  private static SWTWorkbenchBot bot;
+
+  @BeforeClass
+  public static void prepareBot() {
+    bot = new SWTWorkbenchBot();
+    bot.viewByTitle("Welcome").close();
+  }
+
+  @Test
+  public void opened_view_should_load_a_browser() {
+    bot.menu("Window").menu("Show View").menu("Other...").click();
+    var shell = bot.shell("Show View");
+    shell.activate();
+    bot.tree().expandNode("Cody").select("Cody Chat");
+    bot.button("Open").click();
+
+    bot.sleep(3_000); // wait 3 seconds
+    var browser =
+        bot.browser(); // there shouldn't be any other browser widget, so we can choose the first
+    // encountered
+    assertTrue(browser.isPageLoaded());
+    assertTrue(browser.getText().contains("<title>Cody</title>"));
+  }
 }
